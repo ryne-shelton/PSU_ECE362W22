ccc9dfc706d74bae10ac8cecb9df3a8713e568af
Ryne <ryn2@pdx.edu>
2022-01-25 16:34:01 -0800

    Update timer.c
    added some code to get an idea of what to do

--- assignment3/timer.c
+++ assignment3/timer.c
@@ -16,13 +16,35 @@
  
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+
+void err_sys(char *msg)
+{
+	fprintf(stderr,"%s (%s)\n",msg,strerror(errno));
+	exit(-1);
+}
 
 int main(int argc, char *argv[]){
+	time_t start, end, execute_time;
 	char user_input[50];
 	for (int i=0; i<argc-1; i++){
 		strcat(user_input," ");
 		strcat(user_input,*++argv);
 	}
 	printf("input: %s\n", user_input);
+	if(write(STDIN_FILENO,user_input,strlen(user_input)) != strlen(user_input)){
+			err_sys("write to STDOUT error");
+		}
+	start = time(NULL);
+	system(user_input);
+	end = time(NULL);
+	execute_time = end - start;
+	printf("\nSeconds to execute: %ld\n", execute_time);
+	return 0;
 }
