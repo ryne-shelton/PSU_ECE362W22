894c7dff781a2b1a0588ed307c7732cbf606eb2b
Ryne <ryn2@pdx.edu>
2022-01-30 17:08:47 -0800

    Update piper.c
    just needs the arithmetic programed

--- assignment3/piper.c
+++ assignment3/piper.c
@@ -13,6 +13,9 @@
  *
  *
  */
+//=============================================================================================
+// Libraries 
+//=============================================================================================
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -21,35 +24,67 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+//=============================================================================================
+// Macros
+//=============================================================================================
 #define BUFFSIZE 	4096
-
+//=============================================================================================
+// Helper Function 
+//=============================================================================================
 void err_sys(char *msg)
 {
 	fprintf(stderr,"%s (%s)\n",msg,strerror(errno));
 	exit(-1);
 }
-
+//=============================================================================================
+// Main Program
+//=============================================================================================
 int main()
 {
 	// variable declarations
-	char	buff0[BUFFSIZE], buff1[2*BUFFSIZE];  // two data buffers for passing things
+	char	buff0[BUFFSIZE],buff1[BUFFSIZE];  // two data buffers for passing things
 	// fd[0] - parent/child0 pipe, fd[1] - parent/child1 pipe
-	int 	fd[2][2], nread; // fd[][0] - read , fd[][1] - write
-	
+	int 	i, fd[2][2],read_fd, write_fd, nread; // fd[][0] - read , fd[][1] - write
+	char* user_input[50];
+	char* filename = "user_input";
+//=============================================================================================
+// Takes user input from stdin and writes it to a temporary file one line at a time, reads in
+// the temporary file once user singals that they are finished entering input (ctrl-d) to a 
+// buffer (buff1) and then deletes the temporary file (user_input).
+//=============================================================================================
+	if((write_fd = open(filename, O_RDWR | O_CREAT | O_APPEND, S_IRWXU)) < 0){
+		err_sys("open error");
+	}
 	// read input into buffer until ctrl-d ** needs work 
 	while((nread=read(STDIN_FILENO, buff0, BUFFSIZE)) > 0){
-		strcat(buff1,buff0);					
-		printf("\nbuff1: %s\n",buff1);
-		printf("\nbuff0: %s\n",buff0);
-		continue;
+		// write to file
+		if(write(write_fd,buff0,nread) != nread){
+			err_sys("write to file error");
+		}
 	}
-	if(nread<0) err_sys("read error"); 
-	
+	if(nread<0) err_sys("read error");
+	close(write_fd); 			// closes temporary file
+	// Opens file in read only mode
+	if((read_fd = open(filename, O_RDONLY)) < 0){
+		err_sys("open error");
+	}
+	// reads temp file into buff1
+	if((nread = read(read_fd, buff1,BUFFSIZE)) < 0 ){
+		err_sys("read error"); 	// error if read fails
+	}
+	close(read_fd); 			// closes temporary file
+	system("rm user_input"); 	// deletes temporary file
+//=============================================================================================	
+// Creates Pipes
+//=============================================================================================
 	for(int i=0;i<2;i++){
 		if(pipe(fd[i])<0) err_sys("\npipe error");	// open pipes
 	}
-	int child0 = fork();	// Creates child0
+//=============================================================================================
+// Creates Child0 that recieves buff1 from parent then summs the values and prints out the 
+// sum. 
+//=============================================================================================
+	int child0 = fork();	// Creates child0 (addition)
 	if(child0<0) err_sys("\nfork error");	
 	if(child0==0){			// Child process 0	
 		// close unused pipes
@@ -60,11 +95,31 @@ int main()
 			err_sys("\nread error");
 		}
 		close(fd[0][0]);				// close read end at child0 from parent
- 		printf("\nBuffer read in from Parent in Child0 process: %s\n",buff1);	// print was was read from parent 
-		//add code here to perform addition on buffer
+		for (char *token = strtok(buff1," \n"); token != NULL; token = strtok(NULL, " \n")) {
+			user_input[i] = token; 
+			i++;
+		}
+//?????????????????????????????????????????????????????????????????????????????????????????????
+		// DEBUGGER CODE
+		printf("\n\n");
+		for(int j=0; j<i;j++){
+			printf("child 0 user_input[%d]: %s\n",j,user_input[j]);
+		}
+//?????????????????????????????????????????????????????????????????????????????????????????????
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+		//add code here to perform addition on user_input 
+		
+		
+		
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 		return 0;
 	}	
-	int child1 = fork();	// Creates child1
+//=============================================================================================
+// Creates Child1 that recieves buff1 from parent then multiples the values and prints out the 
+// product. 
+//=============================================================================================
+	int child1 = fork();	// Creates child1 (multiplication)
 	if(child1<0) err_sys("\nfork error");	
 	if(child1==0){			// Child process 1
 		// close unused pipes
@@ -75,10 +130,29 @@ int main()
 			err_sys("\nread error");
 		}
 		close(fd[1][0]);				// close read at child 1 from parent 
-		printf("\nBuffer read in from Parent in Child1 process: %s\n",buff1);	// print was was read from parent
+		for (char *token = strtok(buff1," \n"); token != NULL; token = strtok(NULL, " \n")) {
+			user_input[i] = token; 
+			i++;
+		} 
+//?????????????????????????????????????????????????????????????????????????????????????????????
+		// DEBUGGER CODE
+		printf("\n\n");
+		for(int j=0; j<i;j++){
+			printf("child 1 user_input[%d]: %s\n",j,user_input[j]);
+		}
+//?????????????????????????????????????????????????????????????????????????????????????????????
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 		// add code here to perfrom multiplication 
+		
+		
+		
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 		return 0;
 	}
+//=============================================================================================
+// Parent process that writes buff1 to both of its children 
+//=============================================================================================
 	// Parent process
 	close(fd[0][0]);	// close read at child0 from parent
 	close(fd[1][0]);	// close read at child 1 from parent 
@@ -93,54 +167,3 @@ int main()
 	return 0; 
 }
 
-/* 
-int main(int argc, char *argv[]){
-    int n, fd[2], line1[MAXLINE];
-    pid_t Child1, Child2;
-    char *line[MAXLINE]; 
-    
-    if (pipe(fd) < 0){ // make pipe (parent) 
-		err_sys("\npipe error");
-	}      
-    if ((Child1 = fork()) < 0){    // fork it
-        err_sys("\nfork error");
-    } else if (Child1 > 0){ 
-        //int num = read(fd[0], line , MAXLINE); // parent reading from pipe 
-        close(fd[0]); // parent closes reading end
-        for (int i=0; i<argc-1; i++){
-		    strcat(line,*++argv); // adds arguements to string
-        }
-        write(fd[1], line , sizeof(line)); // parent writes to pipe
-    } else{
-        close(fd[1]); // child1 closes writing end
-        n = read(fd[0], line, MAXLINE); // child1 reads from the pipe
-        char* token = strtok(line," \n");
-        while ((token != NULL)){
-                int number = atoi(token); 
-                printf("%d\n", number);
-                token = strtok(NULL," \n");
-        }
-        //for (int i = 0; i <= strlen(*line); i++){
-        //    if ((strcmpline[i] =="\n") || (line[i] == " ")){
-        //        line[i++];
-        //        line1[i] =+ atoi(line[i]);
-        //    }
-        //    line1[i] =+ atoi(line[i]);
-        //} 
-        write(fd[1], line1, n); //child1 writes to pipe
-    }
-    exit(0);
-    //printf("hello world (pid:%d)\n", (int) getpid());
-    //int rc = fork();
-    //if (rc < 0){    // fork failed, then exit
-    //    fprintf(stderr, "fork failed\n");
-    //    exit(1);
-    //} else if (rc == 0){     // child (new process)
-    //    printf("hello, I am child (pid:%d)\n", (int) getpid());
-    //} else {                // parent goes down this path (main)
-    //    printf("hello, I am parent of %d (pid:%d)\n",
-    //            rc, (int) getpid());
-}
- 
-
-  */
